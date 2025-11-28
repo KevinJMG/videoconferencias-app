9c652d43126d47ee7523c6fb01c949801ad553f1
JoseHMontes <jose.montes@correounivalle.edu.co>
2025-11-27 22:38:36 -0500

    solucionando duplicado

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode, useEffect } from 'react';
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router.tsx'
-import './index.css'
-import useAuthStore from './stores/useAuthStore'
-
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router.tsx';
+import './index.css';
+import useAuthStore from './stores/useAuthStore';
 
 function AppWrapper() {
   const initAuthObserver = useAuthStore((state) => state.initAuthObserver);
@@ -20,6 +19,5 @@ function AppWrapper() {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppWrapper />
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
